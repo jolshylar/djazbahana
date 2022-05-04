fe9eae720f031f64195a0910c9b31d2cfe1e6ceb
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-05-04 20:51:17 +0600

    ✔ Fix: `createsuperuser`
    It had username argument required so we needed to specify

--- base/models.py
+++ base/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
     avatar = models.ImageField(null=True, default="avatar.svg", upload_to="avatars/")
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
 
 class Topic(models.Model):
