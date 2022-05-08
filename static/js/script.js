9cc5a450298c06a9f23167419e1fa1730b08224e
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-05-09 00:51:21 +0600

    🚀 Refactor site style

--- static/js/script.js
+++ static/js/script.js
@@ -20,40 +20,37 @@ if (photoInput)
 
 // Scroll to Bottom
 const conversationThread = document.querySelector(".classroom__box");
-if (conversationThread) conversationThread.scrollTop = conversationThread.scrollHeight;
+if (conversationThread)
+  conversationThread.scrollTop = conversationThread.scrollHeight;
 
 // Toggle light theme
 const themeSwitcher = document.getElementById("theme-switch");
 
-let logo = document.getElementById("logo")
-themeSwitcher.checked = false;
+let logo = document.getElementById("logo");
+themeSwitcher.checked = true;
 
 function toggleTheme() {
-  if (this.checked) {
-    // document.body.setAttribute('theme', 'dark');
-    document.body.classList.remove("light");
-    document.body.classList.add("dark");
-    localStorage.setItem("theme", "dark");
-    logo.src = "../images/logo_dark.png";
-  } else {
-    // document.body.setAttribute('theme', 'light');
-    document.body.classList.add("light");
-    document.body.classList.remove("dark");
-    localStorage.setItem("theme", "light");
-    logo.src = "../images/logo_light.png";
-  }
+  let curTheme = (this.checked) ? "light" : "dark";
+  let oldTheme = (this.checked) ? "dark" : "light";
+
+  document.body.classList.replace(oldTheme, curTheme);
+  localStorage.setItem("theme", curTheme);
+  logo.src = `../images/logo_${curTheme}.png`;
 }
+
 themeSwitcher.addEventListener("click", toggleTheme);
 
 window.onload = checkTheme();
 
 function checkTheme() {
-  const localStorageTheme = localStorage.getItem("theme")
+  const localStorageTheme = localStorage.getItem("theme");
 
   if (localStorageTheme !== null) {
     document.body.className = localStorageTheme;
     const themeSwitch = document.getElementById("theme-switch");
-    themeSwitch.checked = true;
+    checked = (localStorageTheme === "light") ? true : false;
+    themeSwitch.checked = checked;
+    logo.src = `../images/logo_${localStorageTheme}.png`
   }
 }
 
@@ -64,7 +61,7 @@ let wrapper = document.getElementById("list-wrapper");
 let counter = 3;
 
 function hamburgerMenu() {
-  if(counter%2 == 1) {
+  if (counter % 2 == 1) {
     counter++;
     navi.classList.add("change");
     wrapper.classList.remove("list-wrapper");
@@ -73,4 +70,4 @@ function hamburgerMenu() {
     navi.classList.remove("change");
     wrapper.classList.add("list-wrapper");
   }
-};
+}
