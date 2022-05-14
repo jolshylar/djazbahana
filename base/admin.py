646bfefad244164972e43293addd40b72a8838df
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-05-14 19:16:28 +0600

    ✅ Add `Conspect` models to `Admin` panel

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import User, Topic, Classroom, Message
+from .models import Classroom, Conspect, Message, Topic, User
 
 admin.site.register(User)
 admin.site.register(Topic)
 admin.site.register(Classroom)
 admin.site.register(Message)
+admin.site.register(Conspect)
